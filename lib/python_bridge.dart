e20e12d56d924b8e98b051a92d284909ac144b3a
Jacob <malin146@umn.edu>
2026-02-25 16:32:27 -0600

    Changed build command

--- lib/python_bridge.dart
+++ lib/python_bridge.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:serious_python/serious_python.dart';
+// import 'package:serious_python/serious_python.dart';
 
 class OmnitermInteraction {
   static Future<bool> fillOutCount() async {
@@ -14,11 +14,12 @@ Future<String> runPythonScript(
   String scriptName, {
   Map<String, String>? args,
 }) async {
-  final String? result = await SeriousPython.run(
-    'app/app.zip',
-    appFileName: scriptName,
-    environmentVariables: args,
-  );
-  if (result == null) throw Exception('No response from serious_python plugin');
-  return result;
+  // final String? result = await SeriousPython.run(
+  //   'app/app.zip',
+  //   appFileName: scriptName,
+  //   environmentVariables: args,
+  // );
+  // if (result == null) throw Exception('No response from serious_python plugin');
+  // return result;
+  return 'aaa';
 }
