70d9263046374e57c2403036f0a6fadea4cf74c9
Jacob Malin <34765120+JacobMalin@users.noreply.github.com>
2026-03-15 19:13:58 -0500

    Add import for flutter/services.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'companion/companion_app.dart';
